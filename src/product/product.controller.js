19a1df94a713efc49f7cb02a9e957e516ae716e6
Matt Fellows <matt.fellows@onegeek.com.au>
2021-05-03 22:30:56 +1000

    feat: add POST /products endpoint

--- src/product/product.controller.js
+++ src/product/product.controller.js
@@ -1,7 +1,13 @@
+const Product = require("./product");
 const ProductRepository = require("./product.repository");
 
 const repository = new ProductRepository();
 
+exports.create = async (req, res) => {
+    const data = req.body
+    const product = new Product(data.id, data.type, data.name, data.version, data.price)
+    product ? res.send(product) : res.status(400).send({message: "invalid product"})
+};
 exports.getAll = async (req, res) => {
     res.send(await repository.fetchAll())
 };
--- src/product/product.routes.js
+++ src/product/product.routes.js
@@ -3,5 +3,6 @@ const controller = require('./product.controller');
 
 router.get("/product/:id", controller.getById);
 router.get("/products", controller.getAll);
+router.post("/products", controller.create);
 
 module.exports = router;
