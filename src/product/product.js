f487595045231e7371d071638153b99a6aa8714d
Matt Fellows <matt.fellows@onegeek.com.au>
2022-07-01 12:56:00 +1000

    demo: remove name property

--- src/product/product.js
+++ src/product/product.js
@@ -2,7 +2,7 @@ class Product {
     constructor(id, type, name, version, price) {
         this.id = id;
         this.type = type;
-        this.name = name;
+        // this.name = name;
         this.version = version;
         this.price = price;
     }
